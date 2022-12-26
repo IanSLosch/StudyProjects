85383818bb4dd3b08ef9868811bb75bfa3f6d01b
Ian S <ianslosch@gmail.com>
2022-12-26 16:20:59 -0500

    first commit

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,7 +2,16 @@
 // If the number is evenly divisible by 3, console log "Fizz"
 // If the number is even divisible by 5, console log "Buzz"
 
-let input = (15)
+/*
+
+let input = ()
 const fizz = Number.isInteger(input/3) ? 'fizz' : ''
 const buzz = Number.isInteger(input/5) ? 'buzz' : ''
 console.log(`${fizz}${buzz}`)
+
+*/
+
+let fb = (i) => {
+  console.log((i % 3 ? 'fizz' : '') + (i % 5 ? 'buzz' : ''))
+}
+fb(15)
--- sort.js
+++ sort.js
@@ -4,7 +4,60 @@
 //
 // Example input: [3, 4, 6, 14, 8]
 // Example output: "3, 4, 6, 8, 14"
+// Can't use Sort
 
+/*
 const integers = [19, 43, 15, 71, 8, 29]
 function ascendingSort(a, b) {return a - b}
 console.log(integers.sort(ascendingSort).join())
+*/
+const input = [3, 4, 6, 14, 8]
+/*
+function bubbleSort(array) {
+    var done = false;
+    while (!done) {
+      done = true;
+      for (var i = 1; i < array.length; i += 1) {
+        if (array[i - 1] > array[i]) {
+          done = false;
+          var tmp = array[i - 1];
+          array[i - 1] = array[i];
+          array[i] = tmp;
+        }
+      }
+    }
+  
+    return array;
+  }
+  const output = bubbleSort(input)
+*/
+
+function bubbleSort(array) {
+  let done = false
+  while (!done) {
+    done = true
+    for (var i = 1; i < array.length; i += 1) {
+      if (array[i - 1] > array[i]) {
+        done = false
+        var tmp = array[i - 1]
+        array[i - 1] = array[i]
+        array[i] = tmp;
+      }
+    }
+  }
+
+  return array
+
+}
+function arrayToString(arr) {
+  let str = '';
+  arr.forEach(function(i, index) {
+    str += i;
+    if (index != (arr.length - 1)) {
+      str += ', ';
+    };
+  });
+  return str;
+}
+
+console.log(`"${arrayToString(bubbleSort(input))}"`)
